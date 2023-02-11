7b83e02e9d8894333299f9fcc5a66ea03637d43a
guen joy alimodian <guenjoyalimodian@guens-MacBook-Air.local>
2023-02-11 18:12:27 +0800

    guest profile update

---
+++ week9/guests.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<div class="sidenav">
+  <a href="index.php">Go Back to My Profile</a>
+</div>
+
+<?php
+$servername = "localhost";
+$username = "webprogmi211";
+$password = "webprogmi211";
+$dbname = "webprogmi211";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, name, email, comment FROM gbalimodian_MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	echo "<h1>Guests Log</h1>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo 
+		"id: ". $row["id"]. 
+		" - Name: ". $row["name"]. 
+		" - Email: ". $row["email"]. 
+		" - Comment: ". $row["comment"]. 
+		"<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
+</body>
+</html>
